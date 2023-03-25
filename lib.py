bb0042f08b360668e68b18e83adb7638ba1f15a0
Lorenzo <47902610+lorenzua02@users.noreply.github.com>
2023-03-25 10:08:18 +0100

    improved mode

--- lib.py
+++ lib.py
@@ -1,10 +1,7 @@
-from collections import defaultdict
+from collections import Counter
 import numpy as np
 
 
 def mode(a: np.ndarray | list) -> int:
-    counter = defaultdict(int)
-    for value in a:
-        counter[value] += 1
-    ordered = sorted(counter.items(), key=lambda x: x[1], reverse=True)
-    return ordered[0][0]
+    count = Counter(a)
+    return max(count, key=count.get)
